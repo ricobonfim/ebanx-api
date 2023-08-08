8e2f24efe1f284d8ad5809a0a5a56262d991bb62
Ricardo Bonfim <ricobonfim@gmail.com>
2023-08-08 11:47:41 -0300

    Remove unnecessary comments

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,5 @@
 class AccountsController < ApplicationController
-  # Get balance for non-existing account
-  # GET /balance?account_id=1234
-  # 404 0
-
-  # Get balance for existing account
-  # GET /balance?account_id=100
-  # 200 20
+  # GET /balance
   def balance
     account = Account.find(balance_params['account_id'])
 
@@ -14,29 +8,7 @@ class AccountsController < ApplicationController
     render status: 404, json: 0
   end
 
-  # Create account with initial balance
-  # POST /event {"type":"deposit", "destination":"100", "amount":10}
-  # 201 {"destination": {"id":"100", "balance":10}}
-
-  # Deposit into existing account
-  # POST /event {"type":"deposit", "destination":"100", "amount":10}
-  # 201 {"destination": {"id":"100", "balance":20}}
-
-  # Withdraw from non-existing account
-  # POST /event {"type":"withdraw", "origin":"200", "amount":10}
-  # 404 0
-
-  # Withdraw from existing account
-  # POST /event {"type":"withdraw", "origin":"100", "amount":5}
-  # 201 {"origin": {"id":"100", "balance":15}}
-
-  # Transfer from existing account
-  # POST /event {"type":"transfer", "origin":"100", "amount":15, "destination":"300"}
-  # 201 {"origin": {"id":"100", "balance":0}, "destination": {"id":"300", "balance":15}}
-
-  # Transfer from non-existing account
-  # POST /event {"type":"transfer", "origin":"200", "amount":15, "destination":"300"}
-  # 404 0
+  # POST /event
   def event
     account_event = AccountServiceFactory.create(event_params)
     @accounts = account_event.handle
@@ -45,9 +17,7 @@ class AccountsController < ApplicationController
     render status: 404, json: 0
   end
 
-  # Reset state before starting tests
   # POST /reset
-  # 200 OK
   def reset
     Account.all.delete_all
 
